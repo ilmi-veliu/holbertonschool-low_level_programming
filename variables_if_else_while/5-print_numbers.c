2749bdf87101463fc72b43f8afa786889a08327d
ilmi-veliu <iveliu011@gmail.com>
2025-02-17 14:53:19 +0000

    5. Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints number
+ * Return: 0
+ */
+int main(void)
+{
+int number = 0;
+
+	while (number <= 10)
+	{
+		putchar(number + '0');
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
