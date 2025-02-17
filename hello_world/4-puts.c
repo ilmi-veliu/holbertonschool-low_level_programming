8ae7a991465bbd89f47f0d720fd776f0adc9c235
ilmi-veliu <iveliu011@gmail.com>
2025-02-17 09:09:02 +0000

    4. Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 int main(void)
+
 /**
  * main - Entry point
  *
+ * Description: This program prints a sentence using the `puts` function.
+ * It demonstrates basic output in C.
+ *
  * Return: Always 0 (Success)
  */
-{	
-	puts ("Programming is like building a multilingual puzzle/n");
-	return(0);
+{
+	puts("\"Programming is like building a multilingual puzzle/n");
+	return (0);
 }
