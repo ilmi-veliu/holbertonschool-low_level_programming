f112f2f0f34fcc677cd050cdc665763b120797f2
ilmi-veliu <iveliu011@gmail.com>
2025-02-18 08:26:59 +0000

    6. Numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Prints number
+ * Return: 0
+ */
 int main(void)
 {
 	int number = 0;
 
-	while(number < 10)
+	while (number < 10)
 	{
 		putchar(number + '0');
 		number++;
