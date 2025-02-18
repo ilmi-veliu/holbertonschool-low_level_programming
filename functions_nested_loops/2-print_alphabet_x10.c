6d9e05a6e1f1c0eaac00c82c7b3754f25c091bb4
ilmi-veliu <iveliu011@gmail.com>
2025-02-18 13:24:47 +0000

    tete

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - Prints alp ten
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+int i , alp;
+		for (i = 0; i < 10 ; i++)
+		{
+			for ( alp = 'a'; alp <= 'z' ; alp++)
+			{
+				_putchar(alp);
+				alp++;
+			}
+
+		}
+
+		_putchar('\n');
+}
