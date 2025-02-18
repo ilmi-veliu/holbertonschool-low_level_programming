fc0bf494b9f4fbd3379759dd61b4ee6440a06479
ilmi-veliu <iveliu011@gmail.com>
2025-02-18 09:38:03 +0000

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - all number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar(number + '0');
+		putchar(',');
+		putchar(' ');
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
