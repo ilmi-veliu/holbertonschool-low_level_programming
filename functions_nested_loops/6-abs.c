59a702b101fe245a511bdd0fd0817ee3294350ae
ilmi-veliu <ilmi011@gmail.com>
2025-02-19 09:08:34 +0000

    absolut number

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,10 +1,16 @@
 #include "main.h"
+/**
+ * _abs - Calcule la valeur absolue d'un entier
+ * @r: L'entier à traiter
+ *
+ * Return: La valeur absolue de r
+ */
 int _abs(int r)
-{	
+{
 	int valeur_absolut;
 
 	valeur_absolut = abs(r);
 	_putchar(valeur_absolut + '0');
 
-	return(valeur_absolut);
+	return (valeur_absolut);
 }
