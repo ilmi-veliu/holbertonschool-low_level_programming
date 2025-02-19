037fc0baaf5068c9420228b0bca4d29bc3d19650
ilmi-veliu <ilmi011@gmail.com>
2025-02-19 09:06:08 +0000

    absolut number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+int _abs(int r)
+{	
+	int valeur_absolut;
+
+	valeur_absolut = abs(r);
+	_putchar(valeur_absolut + '0');
+
+	return(valeur_absolut);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -9,6 +10,7 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
 
 #endif
 
