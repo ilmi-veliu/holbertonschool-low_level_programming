1ac85bd366fc15f35d8f55ad7734b278b372c0d7
ilmi-veliu <ilmi011@gmail.com>
2025-02-19 10:12:41 +0000

    last number

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,16 @@
 #include "main.h"
+/**
+ * _abs - te
+ * @r: L'entier à traiter
+ *
+ * Return: tkt
+ */
 int print_last_digit(int nbr)
 {
-	int last = nbr %10;
+	int last = nbr % 10;
+
+	if (last < 0)
+	last = -last;
 
 	_putchar(last + '0');
 
