196f12a3e70e64ecc5697f98b5b7b458d19990bf
ilmi-veliu <iveliu011@gmail.com>
2025-02-20 20:47:38 +0000

    time

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,12 +1,12 @@
 #include "main.h"
-void times_table(void);
+void times_table(void)
 {
-	int i ,o;
-	int resultat = 
+	int i ,o; 
 
-		for (i = 0; i < 10; i++)
-		{
+		for (i = 0; i != 9; i++)
+		
+			for (o = 0 ; o !=i9)
+			{
 			
-		}
 	
 }
