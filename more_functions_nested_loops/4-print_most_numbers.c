40ecf73fff44d310920000aa584b5a49ed7e9fba
ilmi-veliu <iveliu011@gmail.com>
2025-02-24 09:57:13 +0000

    4. I believe in numbers and signs

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - Affiche les chiffres de 0 à 9 sauf 2 et 4
+ */
+void print_most_numbers(void)
+{
+	int nb = 0;
+
+	while (nb <= 9)
+	{
+		if (nb != 2 && nb != 4)
+			{	_putchar(nb + '0');
+				nb++;
+			}
+	}
+	_putchar('\n');
+}
