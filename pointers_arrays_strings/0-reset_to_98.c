c7cdc449c92215016d9c4a83fb8fdceec72c4d67
ilmi-veliu <iveliu011@gmail.com>
2025-02-25 13:25:12 +0000

    test

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,9 @@
 #include "main.h"
+/**
+ * reset_to_98 - Met à jour la valeur d'un entier à 98
+ * @n: Pointeur vers l'entier à modifier
+ */
 void reset_to_98(int *n)
 {
-	int *n = 98;
+	*n = 98;
 }
