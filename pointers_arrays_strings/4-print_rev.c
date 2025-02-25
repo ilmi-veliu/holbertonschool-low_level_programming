20267f591ec6303ae6cc6b26c453e352463c3f29
ilmi-veliu <iveliu011@gmail.com>
2025-02-25 21:11:36 +0000

    reverse string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,8 @@
 #include "main.h"
+/**
+ * print_rev - Affiche une chaîne inversée
+ * @s: Chaîne à afficher
+ */
 void print_rev(char *s)
 {
 	int i = 0;
@@ -8,7 +12,7 @@ void print_rev(char *s)
 		i++;
 	}
 
-	while (i > 0 )
+	while (i > 0)
 	{
 		i--;
 		_putchar(s[i]);
