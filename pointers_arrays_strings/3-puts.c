4fe33587cdc9f3236f120535262639d4b69a6879
ilmi-veliu <iveliu011@gmail.com>
2025-02-25 20:53:51 +0000

    puts the string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,9 +1,13 @@
 #include "main.h"
+/**
+ * _puts - Affiche une chaîne de caractères
+ * @str: La chaîne à afficher
+ */
 void _puts(char *str)
 {
 	int c;
 
-	for ( c = 0; str[c] != '\0';str++)
+	for (c = 0; str[c] != '\0'; str++)
 	{
 		_putchar(str[c]);
 	}
