b5a37c05b4ad81c8870b0476ad14bab51cf8837e
ilmi-veliu <iveliu011@gmail.com>
2025-02-25 12:51:37 +0000

    test

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,36 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * main - Programme qui affiche les nombres de 1 à 100.
+ *        Remplace les multiples de 3 par "Fizz",
+ * Return: 0 (Succès)
+ */
 int main(void)
 {
-	
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % = 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 5 = 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
 }
