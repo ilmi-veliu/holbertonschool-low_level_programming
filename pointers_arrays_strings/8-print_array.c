b8fd158f9744ba9350d7ba19714b0ddf7af81908
ilmi-veliu <iveliu011@gmail.com>
2025-02-27 19:55:11 +0000

    8. Arrays are not pointers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - Affiche n éléments d'un tableau d'entiers
+ * @a: Tableau d'entiers
+ * @n: Nombre d'éléments à afficher
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d" , a[i]);
+
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
