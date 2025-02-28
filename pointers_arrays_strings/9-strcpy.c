ab553183e40f26dd24009ed7fd0fe3973d78a1ae
ilmi-veliu <iveliu011@gmail.com>
2025-02-28 08:59:39 +0000

    test avec le pointeur du main

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,14 @@
 #include "main.h"
+/**
+ * _strcpy - Copie une chaîne de caractères
+ * @dest: Destination
+ * @src: Source
+ *
+ * Return: Pointeur vers dest
+ */
 char *_strcpy(char *dest, char *src)
 {
-	char *ptr = dest;
+	char *pvd = dest;
 
 	while (*src)
 	{
@@ -10,5 +17,5 @@ char *_strcpy(char *dest, char *src)
 		dest++;
 		src++;
 	}
-	return (ptr);
+	return (pvd);
 }
