287296a2922606b75c35397112d6a88ed2889d33
ilmi-veliu <iveliu011@gmail.com>
2025-03-02 21:17:28 +0000

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * cap_string - Capitalizes all words of a string
  * @str: The string to be modified
@@ -14,23 +15,23 @@ char *cap_string(char *str)
 	while (str[i] != '\0')
 	{
 		if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - ('a' - 'A');
-			}
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
 
 		for (j = 0; separators[j] != '\0'; j++)
 		{
 			if (str[i] == separators[j])
-				{
-					capitalize_next = 1;
-					break;
-				}
-		else
-		{
-			capitalize_next = 0;
-		}
+			{
+				capitalize_next = 1;
+				break;
+			}
+			else
+			{
+				capitalize_next = 0;
+			}
 		}
 		i++;
 	}
-return (str);
+	return (str);
 }
