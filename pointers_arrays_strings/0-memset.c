bbd954efc891055166d34e013c1874e47308eff4
ilmi-veliu <iveliu011@gmail.com>
2025-03-03 10:17:32 +0100

    0. memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,15 @@
 #include "main.h"
 char *_memset(char *s, char b, unsigned int n)
 {
+    int i = 0;
 
+    while (n != 0)
+    {
+        s[i] = b;
+        i++;
+        n--;
+    }
+    
+    return (s);
+    
 }
