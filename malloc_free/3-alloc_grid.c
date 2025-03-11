3237497161fc6eb28926b6d41761e8a8b1c3b652
root <iveliu011@gmail.com>
2025-03-11 13:34:01 +0000

    3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * alloc_grid - Alloue un tableau 2D de dimensions width x height.
@@ -9,42 +10,36 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **tab2;
-	int i, j = 0;
+int **tab2;
+int i, j;
 
-	if (width <= 0 || height <= 0)
-	return (NULL);
+if (width <= 0 || height <= 0)
+return (NULL);
 
-	tab2 = malloc(height * sizeof(int *));
-	if (tab2 == NULL)
-	return (NULL);
+tab2 = malloc(height * sizeof(int *));
+if (tab2 == NULL)
+return (NULL);
 
-	for (i = 0; i < height; i++)
-	{
-		tab2[i] = malloc(width * sizeof(int));
-		if (tab2[i] == NULL)
-		{
-			for (j = 0; j < i; j++)
-			free(tab2[j]);
-			free(tab2);
-			return (NULL);
-		}
-	}
+for (i = 0; i < height; i++)
+{
+tab2[i] = malloc(width * sizeof(int));
+if (tab2[i] == NULL)
+{
+for (j = 0; j < i; j++)
+free(tab2[j]);
+free(tab2);
+return (NULL);
+}
+}
 
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-		{
-			tab2[i][j] = 0;
-		}
-	}
+/* Initialisation du tableau à 0 */
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+tab2[i][j] = 0;
+}
+}
 
-	for (i = 0; i < height; i++)
-	{
-		free(tab2[i]);
-	}
-
-	free(tab2);
-
-	return (tab2);
+return (tab2);
 }
