8783c00de8b87512bfe3b7623709765b800bcadb
ilmi-veliu <iveliu011@gmail.com>
2025-03-13 14:08:38 +0100

    0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,18 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - structure pour représenter un chien
- * @name: nom du chien
- * @age: âge du chien
- * @owner: propriétaire du chien
- *
- * Cette structure permet de stocker les informations concernant un chien,
- * y compris son nom, son âge et le nom de son propriétaire.
+ * struct dog - A simple structure to represent a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
  */
-struct dog
+typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
-};
+char *name;
+float age;
+char *owner;
+} dog;
+void init_dog(dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
