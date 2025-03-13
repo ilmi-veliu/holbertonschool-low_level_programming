d22e24f26ff9d3d9b0b94a903a44b6b8898179ad
ilmi-veliu <iveliu011@gmail.com>
2025-03-13 12:26:25 +0100

    3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * array_range - Crée un tableau d'entiers de min à max
+ * @min: Valeur minimale
+ * @max: Valeur maximale
+ *
+ * Return: Pointeur vers le tableau, NULL si échec
+ */
 int *array_range(int min, int max)
 {
     int i;
