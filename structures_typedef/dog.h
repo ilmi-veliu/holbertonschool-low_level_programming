4a3debeb5f34e441cd488c498098e32740aeb096
ilmi-veliu <iveliu011@gmail.com>
2025-03-13 14:03:35 +0100

    changement de type de variable

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - structure pour représenter un chien
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: propriétaire du chien
+ *
+ * Cette structure permet de stocker les informations concernant un chien,
+ * y compris son nom, son âge et le nom de son propriétaire.
+ */
 struct dog
 {
     char *name;
