624435db97ffa4782515e8538bc708287c00c967
ilmi-veliu <iveliu011@gmail.com>
2025-03-13 12:55:59 +0100

    changement de type de variable

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,18 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
-
-/**
- * struct dog - Struc chien
- * @name: nom
- * @age: age
- * @owner: owner
- */
 typedef struct dog
 {
     char *name;
     float age;
     char *owner;
 } dog_t;
-
 #endif
 
