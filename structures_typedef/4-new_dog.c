3d80791255dff035579f3d29c69a1dbfba6477b5
ilmi-veliu <iveliu011@gmail.com>
2025-03-14 16:14:50 +0100

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,12 +1,40 @@
 #include "dog.h"
-#include <string.h>
+
+/**
+ * _strlen - Computes the length of a string
+ * @s: The string
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _strcpy - Copies a string
+ * @dest: Destination buffer
+ * @src: Source string
+ * Return: Pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
 
 /**
  * new_dog - Creates a new dog
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner of the dog
- *
  * Return: Pointer to the new dog_t structure, or NULL if it fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
@@ -21,22 +49,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (!d)
 		return (NULL);
 
-	name_copy = malloc(strlen(name) + 1);
+	name_copy = malloc(_strlen(name) + 1);
 	if (!name_copy)
 	{
 		free(d);
 		return (NULL);
 	}
-	strcpy(name_copy, name);
+	_strcpy(name_copy, name);
 
-	owner_copy = malloc(strlen(owner) + 1);
+	owner_copy = malloc(_strlen(owner) + 1);
 	if (!owner_copy)
 	{
 		free(name_copy);
 		free(d);
 		return (NULL);
 	}
-	strcpy(owner_copy, owner);
+	_strcpy(owner_copy, owner);
 
 	d->name = name_copy;
 	d->age = age;
