0b01b0ca610505df647c127ea84e82785f5874e9
ilmi-veliu <iveliu011@gmail.com>
2025-03-17 10:34:50 +0100

    0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#inlcude "function_pointers.h"
+/**
+ * print_name - Imprime un nom en utilisant une fonction donnée
+ * @name: Pointeur vers le nom à imprime
+ * @f: Pointeur vers une fonction qui imprime un nom
+ * Return: Rien.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
