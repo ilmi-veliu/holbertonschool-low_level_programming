e9ac8dd1f11001f3a4bc1638ff26a0929c1aacb3
ilmi-veliu <iveliu011@gmail.com>
2025-03-19 11:48:46 +0100

    3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,11 @@
 #include "3-calc.h"
 #include <stdio.h>
+/**
+ * get_op_func - Selects the correct function for the operation.
+ * @s: The operator passed as argument.
+ *
+ * Return: Function pointer for the operation, or NULL if invalid.
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,37 +1,70 @@
-#include <stdio.h>
+#include "3-calc.h"
 #include <stdlib.h>
-
+#include <stdio.h>
+/**
+ * op_add - Calcule la somme de deux nombres
+ * @a: Le premier entier
+ * @b: Le deuxième enti
+ *
+ * Return: La somme de a et b
+ */
 int op_add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
-
+/**
+ * op_sub - Calcule la différence entre deux nombr
+ * @a: Le premier entier
+ * @b: Le deuxième enti
+ *
+ * Return: La différence de a e
+ */
 int op_sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
-
+/**
+ * op_mul - Calcule le produit de deux nombres
+ * @a: Le premier entier
+ * @b: Le deuxième enti
+ *
+ * Return: Le produit de a et b
+ */
 int op_mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
-
+/**
+ * op_div - Calcule la division de deux nombres
+ * @a: Le premier entier
+ * @b: Le deuxième enti
+ *
+ * Return: Le résultat de la division de a pa
+ *         Quitte le programme avec un code d'erreur si b est éga
+ */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a / b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
 }
-
+return (a / b);
+}
+/**
+ * op_mod - Calcule le reste de la division de deux nombres
+ * @a: Le premier entier
+ * @b: Le deuxième enti
+ *
+ * Return: Le reste de la division de a par b
+ *         Quitte le programme avec un code d'erreur si b est égal|
+ */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-	exit(100);
-	}
-	return (a % b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
 }
