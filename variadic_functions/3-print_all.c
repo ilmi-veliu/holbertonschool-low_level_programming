bd5fc14c68da9e155928b4ba4fb068d4492c10c4
ilmi-veliu <iveliu011@gmail.com>
2025-03-21 11:01:29 +0100

    3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * print_char - Imprime un caractère
+ * @args: Liste des arguments
+ */
 void print_char(va_list args)
 {
 	printf("%c", va_arg(args, int));
 }
 
+/**
+ * print_int - Imprime un entier
+ * @args: Liste des arguments
+ */
 void print_int(va_list args)
 {
 	printf("%d", va_arg(args, int));
 }
 
+/**
+ * print_float - Imprime un float
+ * @args: Liste des arguments
+ */
 void print_float(va_list args)
 {
 	printf("%f", va_arg(args, double));
 }
 
+/**
+ * print_string - Imprime une chaîne de caractères
+ * @args: Liste des arguments
+ */
 void print_string(va_list args)
 {
 	char *str = va_arg(args, char *);
@@ -26,6 +42,20 @@ void print_string(va_list args)
 	printf("%s", str);
 }
 
+/**
+ * print_all - Imprime différents types d'arguments selon un format donné
+ * @format: Liste des types d'arguments passés à la fonction
+ * 
+ * Description: La fonction prend en entrée une chaîne `format` et un nombre variable d'arguments.
+ *             Elle affiche chaque argument correspondant au type spécifié dans `format` :
+ *             - 'c' : caractère
+ *             - 'i' : entier
+ *             - 'f' : float
+ *             - 's' : chaîne de caractères
+ * 
+ *             Si un argument de type chaîne de caractères est `NULL`, "(nil)" est affiché à la place.
+ *             La fonction imprime tous les arguments séparés par une virgule et termine par une nouvelle ligne.
+ */
 void print_all(const char * const format, ...)
 {
 	va_list args;
