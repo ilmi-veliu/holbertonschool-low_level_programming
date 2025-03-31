40e00dde3711b9f52856addf1a02afb5a57a926f
ilmi-veliu <iveliu011@gmail.com>
2025-03-31 22:34:10 +0200

    1. List length

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+ * list_len - Counts the number of elements in a linked list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of elements in the list.
+ */
 size_t list_len(const list_t *h)
 {
 	int count = 0;
