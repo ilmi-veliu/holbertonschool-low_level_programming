a1cfe1bea604de7d668cffb40927acfbdb8331e4
ilmi-veliu <iveliu011@gmail.com>
2025-04-01 09:54:26 +0200

    fdf

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,1 +1,14 @@
-#inc
+#include "lists.h"
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *last_node;
+
+	last_node = malloc(sizeof(list_t));
+
+	if (last_node == NULL)
+	return (NULL);
+
+	last_node->str = strdup(str);
+	last_node->len = 
+	
+}
