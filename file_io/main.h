90093c7dcb311d62261c476180e2fd0e3171fdd4
ilmi-veliu <iveliu011@gmail.com>
2025-04-04 09:09:38 +0200

    betty 0

--- file_io/main.h
+++ file_io/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <fcntl.h>    // pour open() et les options comme O_RDONLY
-#include <unistd.h>   // pour read(), write(), close()
-#include <stdlib.h>   // pour malloc(), free()
+#include <fcntl.h>    /* pour open() et les options comme O_RDONLY */
+#include <unistd.h>   /* pour read(), write(), close(), ssize_t */
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
